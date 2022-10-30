8877a8251d717d711323d4c1b08069e3cb83bb88
powersoftdev <90004303+powersoftdev@users.noreply.github.com>
2022-10-30 15:33:55 +0100

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using FirstAssignment.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstAssignment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        List<Home> home = new List<Home>();
+        public HomeController()
+        {
+            home.Add(new Home { SlackUsername = "Azeezat", Backend = true, Age = 25, Bio = "An aspiring software engineer" });
+        }
+
+        [HttpGet]
+        public  IEnumerable<Home> Get()
+        {
+            return home;
+        }
+    }
+}
---
+++ Models/Home.cs
@@ -0,0 +1,10 @@
+﻿namespace FirstAssignment.Models
+{
+    public class Home
+    {
+        public string SlackUsername { get; set; }
+        public bool Backend { get; set; }
+        public int Age { get; set; }
+        public string Bio { get; set; }
+    }
+}
